a14938dea1bc959ec167f69bd2c2d3ed4834acc6
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-01-26 07:56:31 +0100

    base_rest: Fix backport

--- base_rest/models/ir_rule.py
+++ base_rest/models/ir_rule.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -15,7 +16,7 @@ class IrRule(models.Model):
 
     @api.model
     def _eval_context(self):
-        ctx = super()._eval_context()
+        ctx = super(IrRule, self)._eval_context()
         if "authenticated_partner_id" in self.env.context:
             ctx["authenticated_partner_id"] = self.env.context[
                 "authenticated_partner_id"
@@ -24,4 +25,4 @@ class IrRule(models.Model):
 
     def _compute_domain_keys(self):
         """Return the list of context keys to use for caching ``_compute_domain``."""
-        return super()._compute_domain_keys() + ["authenticated_partner_id"]
+        return super(IrRule, self)._compute_domain_keys() + ["authenticated_partner_id"]
