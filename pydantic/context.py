58cdbdf6d0569b821c55224d252fec2647177722
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-02-02 15:31:09 +0100

    pydantic: remove model extension mechanism
    The extension mechanism is now provided by the python package extendable-pydantic https://pypi.org/project/extendable_pydantic/
    The registry initilisation logic is provided by the odoo addon extendable https://github.com/acsone/odoo-addon-extendable

--- pydantic/context.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2021 ACSONE SA/NV
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
-
-# define context vars to hold the pydantic registry
-
-from contextvars import ContextVar
-
-odoo_pydantic_registry = ContextVar("pydantic_registry")
