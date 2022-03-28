3f84ffa477116f4aae7d95628c6e8a536d03d4bb
nans <nans.lefebvre@acsone.eu>
2022-03-28 16:07:51 +0200

    [FIX] rest_log: missing api.model on config parameter create

--- rest_log/models/ir_config_parameter.py
+++ rest_log/models/ir_config_parameter.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models
+from odoo import api, models
 
 
 class IrConfigParameter(models.Model):
     _inherit = "ir.config_parameter"
 
+    @api.model
     def create(self, vals):
         self.clear_caches()
         return super(IrConfigParameter, self).create(vals)
